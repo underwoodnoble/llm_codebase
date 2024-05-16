112a9a2b9a4852afeb8bdc5b053c4aef1b717e08
nobelhu <nobelhu@tencent.com>
2024-05-16 15:47:22 +0800

    update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import os
 from src.utils.args_utils import get_args
 from src.arguments import GenericDataArguments, GenericTrainingArguments
 from src.utils.data_utils import load_dataset
-from src.utils.model_utils import load_tokenizer_and_model
-from src.utils.general_utils import print_object_on_main_process, print_rank_0, get_collator_and_trainer
+from src.utils.model_utils import load_tokenizer_and_model, get_collator_and_trainer
+from src.utils.general_utils import print_object_on_main_process, print_rank_0, set_seed
 
 
 def main(
@@ -14,18 +14,17 @@ def main(
     data_args: GenericDataArguments
     ):
 
+    set_seed(training_args.seed)
     # Loading Dataset
     train_dataset, eval_dataset = load_dataset(data_args, algorithm)
     print_object_on_main_process("train_dataset", train_dataset)
     print_object_on_main_process("eval_dataset", eval_dataset)
-    
 
     # Loading Model
     tokenizer, model, ref_model = load_tokenizer_and_model(training_args, algorithm)
     print_object_on_main_process("tokenizer", tokenizer)
     print_object_on_main_process("model", model)
 
-    
     # Create PEFT model.
     if training_args.peft_config_path is not None:
         from peft import get_peft_model
@@ -36,6 +35,8 @@ def main(
         print_rank_0('>' * 100)
         model.print_trainable_parameters()
         print_rank_0('>' * 100)
+        if training_args.gradient_checkpointing:
+            model.enable_input_require_grads()
 
 
     COLLATOR, TRAINER = get_collator_and_trainer(algorithm)
@@ -43,14 +44,13 @@ def main(
         model=model,
         ref_model=ref_model,
         args=training_args,
-        data_collator=COLLATOR,
+        data_collator=COLLATOR(tokenizer, training_args),
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         tokenizer=tokenizer
     )
     # Operation before training
     
-
     # Train
     trainer.train()
     
