135f00409ebef5155568b465916711906dffa16d
TianhaoHu <tianhaohu@MBP-2CH150MD6P-0241.local>
2024-07-18 19:43:18 +0800

    fix a bug

--- accelerate_inference.py
+++ accelerate_inference.py
@@ -3,6 +3,7 @@ import yaml
 from tqdm import tqdm
 from argparse import ArgumentParser
 from typing import List
+from pathlib import Path
 
 import torch
 from transformers import PreTrainedTokenizer, PreTrainedModel, AutoTokenizer, AutoModelForCausalLM, GenerationConfig
@@ -26,6 +27,15 @@ def get_parser():
     )
     parser.add_argument(
         '--input_file_path', type=str
+    )
+    parser.add_argument(
+        '--prompt_field_name', type=str, default='prompt'
+    )
+    parser.add_argument(
+        '--answers_field_name', type=str, default='answers', help='Only for reward model inference'
+    )
+    parser.add_argument(
+        '--output_field_name', type=str, default='output'
     )
     parser.add_argument(
         '--batch_size', type=int
@@ -84,25 +94,18 @@ def reward_model_inference(dataset: List, tokenizer: PreTrainedTokenizer, model:
 
 
 @torch.no_grad()
-def llm_inference(dataset: List, tokenizer: PreTrainedTokenizer, model: PreTrainedModel, batch_size: int, generation_config: GenerationConfig, save_path: str, add_special_tokens=False):
-    bos_token = tokenizer.bos_token if add_special_tokens and tokenizer.bos_token is not None else ""
-    if 'prompt' in dataset[0]:
-        prompts = [bos_token + data['prompt'] for data in dataset]
-    elif 'query' in dataset[0]:
-        prompts = [bos_token + data['query'] for data in dataset]
-    elif 'input' in dataset[0]:
-        prompts = [bos_token + data['input'] for data in dataset]
-    else: 
-        raise ValueError('wrong data!')
+def llm_inference(dataset: List, tokenizer: PreTrainedTokenizer, model: PreTrainedModel,generation_config: GenerationConfig, args):
+    bos_token = tokenizer.bos_token if args.add_special_tokens and tokenizer.bos_token is not None else ""
+    prompts = [bos_token + data[args.prompt_field_name] for data in dataset]
     
 
-    for i in range(0, len(prompts), batch_size):
-        encoding = tokenizer(prompts[i:i+batch_size], add_special_tokens=False, padding=True, truncation=True, return_tensors='pt')
+    for i in range(0, len(prompts), args.batch_size):
+        encoding = tokenizer(prompts[i:i+args.batch_size], add_special_tokens=False, padding=True, truncation=True, return_tensors='pt')
         ret = model.generate(encoding['input_ids'].to(model.device), attention_mask=encoding['attention_mask'].to(model.device), generation_config=generation_config)
         responses = tokenizer.batch_decode(ret, skip_special_tokens=True)
         print(responses)
-        with open(save_path, 'a') as f:
-            for j, prompt in enumerate(prompts[i:i+batch_size]):
+        with open(args.save_path, 'a') as f:
+            for j, prompt in enumerate(prompts[i:i+args.batch_size]):
                 prompt_responses = responses[j*generation_config.num_return_sequences:(j+1)*generation_config.num_return_sequences]
                 f.write(json.dumps({
                     'prompt': prompt,
@@ -114,6 +117,12 @@ def main(args):
     dataset = load_json_or_jsonl(args.input_file_path)
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     tokenizer.model_max_length = args.model_max_length
+
+    # resume inference
+    if Path(args.save_path).exists():
+        with open(args.save_path, 'r') as f:
+            length = len(f.readlines())
+            dataset = dataset[length:]
 
     TYPE_MAP = {
         'fp16': torch.float16,
@@ -135,7 +144,7 @@ def main(args):
             generation_config = GenerationConfig.from_dict(yaml.safe_load(f))
         model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, device_map='auto', torch_dtype=TYPE_MAP[args.dtype])
         model.eval()
-        llm_inference(dataset, tokenizer, model, args.batch_size, generation_config, args.save_path, args.add_special_tokens)
+        llm_inference(dataset, tokenizer, model, generation_config, args)
     
 
 if __name__ == '__main__':
--- multi_process_inference.py
+++ multi_process_inference.py
@@ -1,59 +1,46 @@
 import os
 import subprocess
 import json
-import yaml
 
-from argparse import ArgumentParser
-from pydantic import BaseModel, Field, ConfigDict
 from typing import List
 from pathlib import Path
+from dataclasses import dataclass, field
+
+import torch
+import torch.distributed as dist
+from transformers import TrainingArguments, HfArgumentParser
 
 
-class InferenceConfig(BaseModel):
-    model_config  = ConfigDict(protected_namespaces=())
+def print_rank0(string: str):
+    if dist.is_initialized():
+        if dist.get_rank() == 0:
+            print(string)
+    else:
+        print(string)
+    
+@dataclass
+class InferenceArguments(TrainingArguments):
+    model_type: str = field(default='other')
+    model_name_or_path: str = field(default=None)
+    input_files: List[str] = field(default=None)
+    batch_size_per_process: int = field(default=None)
 
-    processes: int
-    gpus: int
-    model_type: str
-    model_name_or_path: str
-    input_files: List[str]
-    save_dir: str
+    task_type: str = field(default='rm')
+    model_max_length: int = field(default=512)
+    generation_config_path: str = field(default=None)
+    dtype: str = field(default='fp16')
+    add_special_tokens: bool = field(default=False)
 
-    task_type: str = Field(default='rm')
-    batch_size_per_process: int = Field(default=1)
-    model_max_length: int = Field(default=512)
-    generation_config_path: str = Field(default=None)
-    dtype: str = Field(default='fp16')
-    add_special_tokens: bool = Field(default=False)
 
-    @staticmethod
-    def load_config(config_path):
-        with open(config_path, 'r') as f:
-            config = yaml.safe_load(f)
-            config = InferenceConfig(**config)
+    def post_initalize(self):
+        if len(self.input_files) > 1 or dist.get_world_size() > 1:
+            dataset = self.merge_file()
+            self.input_files = self.split(dataset)
+    
 
-        if len(config.input_files) > 1 or config.processes > 1:
-            dataset = InferenceConfig.merge_file(config.input_files)
-            config.input_files = InferenceConfig.split(dataset, config.processes, config.save_dir)
-        
-        if config.gpus % config.processes != 0:
-            raise ValueError('The number of Gpus must be an integer multiple of the number of processes') 
-
-        return config
-
-    def generate_cuda_devices_list(self):
-        gpus_per_process = self.gpus // self.processes
-        return [
-            ','.join([
-                str(j) for j in
-                range(i*gpus_per_process, (i+1)*gpus_per_process)])
-            for i in range(self.processes)
-        ]
-
-    @staticmethod
-    def merge_file(file_paths):
+    def merge_file(self):
         all_dataset = []
-        for file_path in file_paths:
+        for file_path in self.input_files:
             with open(file_path, 'r') as f:
                 if file_path.endswith('json'):
                     dataset = json.load(f)
@@ -61,54 +48,53 @@ class InferenceConfig(BaseModel):
                     dataset = [json.loads(line) for line in f.readlines()]
                 all_dataset.extend(dataset)
         return all_dataset
-    
-    @staticmethod
-    def split(dataset, chuncks, save_dir):
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-        chunck_size = (len(dataset) + 1) // chuncks
+
+    def split(self, dataset):
+        if  dist.get_rank() == 0 and (not os.path.exists(self.output_dir)):
+            os.makedirs(self.output_dir)
+        chunck_size = (len(dataset) + 1) // dist.get_world_size()
+
         new_input_files = []
         for i, offset in enumerate(range(0, len(dataset), chunck_size)):
-            file_path = Path(save_dir) / f'input_rank_{i}.jsonl'
-            with open(file_path, 'w') as f:
-                json.dump(dataset[offset:offset+chunck_size], f, ensure_ascii=False, indent=2)
+            file_path = Path(self.output_dir) / f'input_rank{i}.jsonl'
+            if dist.get_rank() == 0: # Only wirte on main processes
+                with open(file_path, 'w') as f:
+                    json.dump(dataset[offset:offset+chunck_size], f, ensure_ascii=False, indent=2)
             new_input_files.append(str(file_path))
-        return new_input_files
-    
+        return new_input_files        
 
-def get_parser():
-    parser = ArgumentParser()
-    parser.add_argument(
-        '--config_path', type=str
-    )
-    return parser
+    def generate_cuda_devices_list(self):
+        gpus_per_process =  torch.cuda.device_count() // int(os.getenv('LOCAL_WORLD_SIZE'))
+        return [
+            ','.join([
+                str(j) for j in
+                range(i*gpus_per_process, (i+1)*gpus_per_process)])
+            for i in range(int(os.getenv('LOCAL_WORLD_SIZE')))
+        ]
 
 
-def main(args):
+def main(args: InferenceArguments):
+    args.post_initalize()
+    cuda_devices_list = args.generate_cuda_devices_list()
+
     processes = []
-    config = InferenceConfig.load_config(args.config_path)
-    print(config)
-    cuda_devices_list = config.generate_cuda_devices_list()
-
-    if not os.path.exists(config.save_dir):
-        os.mkdir(config.save_dir)
-
     for i, cuda_devices in enumerate(cuda_devices_list):
+        rank = int(os.getenv('RANK')) if dist.is_initialized() else i
         env = os.environ.copy()
         env['CUDA_VISIBLE_DEVICES'] = cuda_devices
 
-        save_path = config.save_dir + f'/rank{i}.jsonl'
+        save_path = args.output_dir + f'/rank{rank}.jsonl'
         cmd_args = [
-            f'--task_type {config.task_type}',
-            f'--model_type {config.model_type}',
-            f'--model_name_or_path {config.model_name_or_path}',
-            f'--model_max_length {config.model_max_length}',
-            f'--input_file_path {config.input_files[i]}',
-            f'--batch_size {config.batch_size_per_process}',
-            f'--generation_config {config.generation_config_path}',
+            f'--task_type {args.task_type}',
+            f'--model_type {args.model_type}',
+            f'--model_name_or_path {args.model_name_or_path}',
+            f'--model_max_length {args.model_max_length}',
+            f'--input_file_path {args.input_files[rank]}',
+            f'--batch_size {args.batch_size_per_process}',
+            f'--generation_config {args.generation_config_path}',
             f'--save_path {save_path}',
-            f'--dtype {config.dtype}',
-            f'--add_special_tokens {config.add_special_tokens}'
+            f'--dtype {args.dtype}',
+            f'--add_special_tokens {args.add_special_tokens}'
         ]
         cmd_list = ['python', str((Path(__file__).parent / 'accelerate_inference.py'))] + cmd_args
         cmd = ' '.join(cmd_list)
@@ -121,6 +107,6 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = get_parser()
-    args = parser.parse_args()
+    parser = HfArgumentParser((InferenceArguments,))
+    args = parser.parse_args_into_dataclasses()[0]
     main(args)
